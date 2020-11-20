82dc873fc6127742c37144ae2a422311dc43dcaa
jeremyt135 <jdt13579@gmail.com>
2020-11-20 00:39:42 -0800

    Remove rust/paint/pkg from .gitignore

---
+++ src/rust/paint/pkg/index.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Brush {
+  free(): void;
+}
+/**
+*/
+export class Painter {
+  free(): void;
+/**
+* @param {HTMLDivElement} canvasTarget
+*/
+  constructor(canvasTarget: HTMLDivElement);
+/**
+* @param {Float32Array} color
+*/
+  changeColor(color: Float32Array): void;
+}
---
+++ src/rust/paint/pkg/paint.js
@@ -0,0 +1,2 @@
+import * as wasm from "./paint_bg.wasm";
+export * from "./paint_bg.js";
